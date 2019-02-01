480ae5c970454f69ef249c89c4ec028a4605e674
Martin Rosko <martin.rosko@gmail.com>
2019-02-02 00:34:54 +0100

    gulp and sass

---
+++ DrBaby/gulpfile.js
@@ -0,0 +1,43 @@
+﻿/// <binding ProjectOpened='sass:watch' />
+/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var replace = require('gulp-replace');
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function (done) {
+    gulp.src('./Styles/**/*.scss')
+        .pipe(sass())
+        .on('error', sass.logError)
+        .pipe(gulp.dest('./Styles/'))
+        .pipe(cleanCSS({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({ extname: ".min.css" }))
+        .pipe(gulp.dest('./Styles/'))
+        .on('end', done);
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./Styles/**/*.scss', ['sass']);
+});
+
+gulp.task('mergeHtmlTemplates', function (done) {
+    gulp.src('./**/*.tmpl.html')
+        .pipe(concat('templates.js'))
+        .on('data', function (file) {
+            gulp.src("./index-template.html")
+                .pipe(replace("<!-- Template Placeholder -->", file.contents.toString()))
+                .pipe(rename({ extname: ".min.css" }))
+                .pipe(gulp.dest('./'))
+                .on('end', done);
+        });
+})
